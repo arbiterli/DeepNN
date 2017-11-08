3bf22e71a64bd1bb720d4dd70a35feb9d5eaedc9
Yuxing Li <yuxing@easyanticheat.net>
2017-11-08 15:05:50 +0200

    [*] keep building CNN

--- CNN.py
+++ CNN.py
@@ -16,8 +16,25 @@ class cnn:
                                      activation=tf.nn.relu)
 
         with tf.variable_scope('pooling1'):
-            pooling1 = tf.layers.max_pooling2d(inputs=conv1,
-                                               pool_size=[2,2],
-                                               strides=2)
+            pooling1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides=2)
+
+        with tf.variable_scope('conv2'):
+            conv2 = tf.layers.conv2d(inputs=pooling1,
+                                     filters=64,
+                                     kernel_size=[5,5],
+                                     padding='same',
+                                     activation=tf.nn.relu())
+
+        with tf.variable_scope('pooling2'):
+            pooling2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], strides=2)
+
+        pool_flat = tf.reshape(pooling2, [-1, 7*7*64])
+        with tf.variable_scope('dense'):
+            dense = tf.layers.dense(inputs=pooling2, units=1024, activation=tf.nn.relu)
+
+        with tf.variable_scope('dropout'):
+            dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=True)
+
+        
 
 
