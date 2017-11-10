cd32397ee60da1c0333b15fa31fa7c71b33c60d8
Yuxing Li <yuxing@easyanticheat.net>
2017-11-10 12:05:23 +0200

    [*] training process is done

--- CNN_model/CNN.py
+++ CNN_model/CNN.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 
-class cnn:
-    def __init__(self, labels, batch_size=128):
-        g = tf.Graph()
-        with g.as_default():
-            self.X = tf.placeholder(tf.float32, shape=(batch_size, 28, 28, 1))
-            self.Y = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
+
+class CNN:
+    def __init__(self, batch_size=100):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            self.images = tf.placeholder(tf.float32, shape=(batch_size, 28 * 28), name='input_images')
+            self.labels = tf.placeholder(tf.float32, shape=(batch_size,), name='input_labels')
+            self.X = tf.reshape(self.images, [batch_size, 28, 28, 1])
+            self.Y = tf.one_hot(indices=tf.cast(self.labels, tf.int32), depth=10)
             self.build_graph()
             self.loss = tf.losses.softmax_cross_entropy(onehot_labels=self.Y, logits=self.output)
             self.optimize()
@@ -26,7 +29,7 @@ class cnn:
                                      filters=64,
                                      kernel_size=[5,5],
                                      padding='same',
-                                     activation=tf.nn.relu())
+                                     activation=tf.nn.relu)
 
         with tf.variable_scope('pooling2'):
             pooling2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], strides=2)
@@ -44,7 +47,4 @@ class cnn:
     def optimize(self):
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
         self.train_op = optimizer.minimize(self.loss, global_step=tf.train.get_global_step())
-
-
-
 
