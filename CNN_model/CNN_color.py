a3822f1b763bdcd1aa1e7e82aa599ddea5c86cd5
Yuxing Li <yuxing@easyanticheat.net>
2018-04-24 10:14:56 +0300

    [*] tensorflow read image file

--- CNN_model/CNN_color.py
+++ CNN_model/CNN_color.py
@@ -3,9 +3,7 @@ import matplotlib.image as mpimg
 
 
 image = '../models/koala.jpg'
-# image_data = tf.gfile.FastGFile(image, 'rb').read()
-# decode_data = tf.image.decode_jpeg(image_data, channels=3)
-# print(decode_data)
-
-image_data = mpimg.imread(image)
-print(image_data.shape)
+image_data = tf.read_file(image)
+decode_data = tf.image.decode_jpeg(image_data, channels=3)
+with tf.Session().as_default():
+    print(decode_data.eval())
